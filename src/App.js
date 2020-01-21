1ed868f67bb061ee99493a177b3a5afae77bfb86
agner <lexxhem@gmail.com>
2020-01-21 19:48:05 +0400

    created first counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Logout from "./components/Logout";
-import Admin from "./components/Admin";
-import uuid from "uuid";
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Counters from "./components/Counters";
+import data from "./data";
 
 function App() {
-  let store = status => {
-    if (status === "set") {
-      sessionStorage.setItem("key", uuid.v4());
-    } else if (status === "remove") {
-      sessionStorage.removeItem("key");
+  let [counters, setCounters] = useState([]);
+  let [amount, setAmount] = useState();
+
+  useEffect(() => {
+    let stored = storage("get");
+    if (stored) {
+      setCounters(stored);
+    } else {
+      let tempCounters = data.map(item => {
+        return { ...item };
+      });
+      setCounters(tempCounters);
     }
-    return sessionStorage.getItem("key");
+  }, []);
+
+  useEffect(() => {
+    handleReduce();
+    storage("set");
+  }, [counters]);
+
+  let storage = status => {
+    if (status === "set") {
+      sessionStorage.setItem("counters", JSON.stringify(counters));
+    } else {
+      return JSON.parse(sessionStorage.getItem("counters"));
+    }
+  };
+
+  let handleReduce = () => {
+    if (counters.length <= 0) return;
+    let values = counters.map(counter => counter.value);
+    let tempAmount = values.reduce((total, num) => total + num);
+    setAmount(tempAmount);
+  };
+
+  let handleDelete = counterId => {
+    let tempCounters = counters.filter(counter => counter.id !== counterId);
+    setCounters(tempCounters);
+  };
+
+  let handleIncrement = (counter, adding) => {
+    let tempCounters = [...counters];
+    let index = tempCounters.indexOf(counter);
+    tempCounters[index] = { ...counter };
+    adding ? tempCounters[index].value++ : tempCounters[index].value--;
+    setCounters(tempCounters);
+    tempCounters[index].value < 0 && handleDelete(counter.id);
+  };
+
+  let handleReset = () => {
+    let tempCounters = data.map(counter => {
+      counter.value = 0;
+      return counter;
+    });
+    setCounters(tempCounters);
+  };
+
+  let all = {
+    counters,
+    onDelete: handleDelete,
+    onIncrement: handleIncrement,
+    onReduce: handleReduce
   };
 
   return (
-    <Router>
-      <Route exact path="/" render={props => <Login store={store} />} />
-      <Route exact path="/admin" render={props => <Admin store={store} />} />
-      <Route path="/logout" component={Logout} />
-    </Router>
+    <>
+      <Navbar amount={amount} />
+      <button className="btn btn-warning m-3" onClick={handleReset}>
+        Reset
+      </button>
+      <Counters {...all} />
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,5 @@
+export default [
+  { id: "one", value: 4 },
+  { id: "two", value: 1 },
+  { id: "three", value: 0 }
+]
